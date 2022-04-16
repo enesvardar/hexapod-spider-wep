f7e708a0a17f1a25d2da48c393db09aedfbfbb4c
enes vardar <enesvrdr0@gmail.com>
2022-04-16 15:32:19 +0300

    REACT_APP_BASE_ENDPOINT api.js dosyasına eklendi

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
+const REACT_APP_BASE_ENDPOINT = " http://localhost:5000"
+
 export const fetchBodyTransform = async(data) => {
-    const result = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/transform`,data);
+    const result = await axios.post(`${REACT_APP_BASE_ENDPOINT}/transform`,data);
     return result;
 }
 
 
-
 export const fetchBodyParameter = async(data) => {
-    const result = await axios.post(`${process.env.REACT_APP_BASE_ENDPOINT}/parameter`,data);
+    const result = await axios.post(`${REACT_APP_BASE_ENDPOINT}/parameter`,data);
     return result;
 }
