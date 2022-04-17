72d196e8fe6980f18c10c0ded76b48aaed21983c
enes vardar <enesvrdr0@gmail.com>
2022-04-17 16:55:11 +0300

    leg bilgilerini güncellemek için eklemelr yapıldır

--- client/src/redux/legs/legsSlice.js
+++ client/src/redux/legs/legsSlice.js
@@ -2,19 +2,30 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const legsSlice = createSlice({
   name: "legs",
-  
+
   initialState: {
     info: [],
   },
 
   reducers: {
-
     setLegs: (state, action) => {
       state.info = action.payload;
     },
-  },
 
+    setLeg: (state, action) => {
+      if(action.payload.angle == "alpha"){
+        state.info[action.payload.index].alpha = action.payload.value;
+      }
+      else if(action.payload.beta == "beta"){
+        state.info[action.payload.index].beta = action.payload.value;
+      }
+      else if(action.payload.angle == "gama"){
+        state.info[action.payload.index].gama = action.payload.value;
+      }
+
+    },
+  },
 });
 
-export const { setLegs } = legsSlice.actions;
+export const { setLegs, setLeg } = legsSlice.actions;
 export default legsSlice.reducer;
